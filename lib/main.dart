4c551d5ff35ddb1d58d2deb6d8bf529835355b0a
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-14 23:26:57 +0500

    📦 NEW: Card and List

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
@@ -35,7 +36,54 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.5,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+92 308 7574575',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'sharjeel924@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
           ],
         )),
       ),

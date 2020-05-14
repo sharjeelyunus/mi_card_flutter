3d345203b91b4f55bbcea95ddd0b6dc949753d20
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-14 19:51:06 +0500

    📦 NEW: Layout and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/sharjeel.jpg'),
+            ),
+            Text(
+              "Sharjeel Yunus",
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal[100],
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
